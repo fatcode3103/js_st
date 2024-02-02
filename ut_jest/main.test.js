6c2671fae62822c0151a08823c98361eb48acdca
fatcode3103 <chuvandat1112003@gmail.com>
2024-02-02 07:58:46 +0700

    addition describe for jest

--- ut_jest/main.test.js
+++ ut_jest/main.test.js
@@ -1,14 +1,19 @@
+// const { describe } = require("yargs");
 const { isOddNumber, cloneArray } = require("./main");
 
-test("should be true if 3", () => {
-    expect(isOddNumber(3)).toBe(true);
+describe("function isOddNumber", () => {
+    test("should be true if 3", () => {
+        expect(isOddNumber(3)).toBe(true);
+    });
+
+    test("should be true if 2", () => {
+        expect(isOddNumber(2)).toBe(false);
+    });
 });
 
-test("should be true if 2", () => {
-    expect(isOddNumber(2)).toBe(false);
-});
-
-test("should be arr if arr", () => {
-    const arr = [1, 2, 3];
-    expect(cloneArray(arr)).toEqual(arr);
+describe("function cloneArray", () => {
+    test("should be arr if arr", () => {
+        const arr = [1, 2, 3];
+        expect(cloneArray(arr)).toEqual(arr);
+    });
 });
